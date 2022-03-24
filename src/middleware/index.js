78adbebef9588da3f956a4311a7a05e704a784bd
Damien R <damien.rembert@gmail.com>
2022-03-24 12:19:46 +0000

    adding middleware from previous project

---
+++ src/middleware/index.js
@@ -0,0 +1,50 @@
+//@ts-check
+
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../user/userTable.js");
+
+exports.hashPassword = async (req, res, next) => {
+    try {
+        // const pass = req.body.pass;
+        // const hashedPass = await bcrypt.hash(pass, 8);
+        // req.body.pass = hashedPass;
+        req.body.pass = await bcrypt.hash(req.body.pass, 8);
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({err: error.message});
+    }
+}
+
+exports.decryptPassword = async (req, res, next) => {
+  try {
+    req.user = await User.findOne({ username: req.body.username });
+    if (await bcrypt.compare(req.body.pass, req.user.pass)) {
+      next();
+    } else {
+      throw new Error("Incorrect credentials");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error.message });
+  }
+};
+
+exports.checkToken = async (req, res, next) => {
+  try {
+    const decodedToken = await jwt.verify(
+      req.header("Authorization").replace("Bearer ", ""),
+      process.env.SECRET
+    );
+    req.user = await User.findOne(decodedToken._id);
+    if (req.user) {
+      next();
+    } else {
+      throw new Error("No user found");
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ err: error.message });
+  }
+};
